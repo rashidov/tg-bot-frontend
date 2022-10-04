91fd4549773e82408b3570555c68bdf21d981d16
arashidov <arashidov@smartdatatech.io>
2022-10-04 22:45:50 +0400

    init
    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+
+// @ts-ignore
+const tg = window.Telegram.WebApp
 
 function App() {
+
+  useEffect(() => {
+    tg.ready()
+  })
+
+  const onClose = () => {
+    tg.close()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hi
+      <button onClick={onClose}>close</button>
     </div>
   );
 }
